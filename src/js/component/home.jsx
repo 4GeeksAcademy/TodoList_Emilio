2c5e140abae2ef5e457bbe3a2073fc9dc1629a54
Emilio Acosta <139580611+Emilioone@users.noreply.github.com>
2023-09-24 19:19:00 +0000

    TodoList

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,58 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const[task, setTask] = useState("")
+	
+
+	const[listTask, setListTask] = useState([])
+
+	const handleChange = (event)=>{
+		setTask(event.target.value)
+	}
+
+	const handleSavetask= (event)=>{
+
+		if(event.key === "Enter"){
+			if(task != ""){
+			setListTask([...listTask, task])
+			setTask("")
+		}else {
+			console.log("Debe llenar los campos")
+		}
+		}
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<>
+		<div className = "container">
+			<div className="row justify-content-center">
+				<div className="col-12 col-md-6">
+					<h1>Mi lista de tareas</h1>
+
+					<form onSubmit={(event) => event.preventDefault()} > 
+						<input 
+						type="text"
+						name="label"
+						className="form-control" 
+						placeholder="Agrega una tarea" 
+						value={task} 
+						onChange={handleChange}
+						onKeyDown={handleSavetask}
+			
+						/>
+					</form>
+					<ul>
+						{listTask.map((item, index)=>{
+							return (
+								<li key={index}>{item}</li>
+							)
+						})}
+					</ul>
+				</div>
+			</div>
 		</div>
+	   </>	
 	);
 };
 
